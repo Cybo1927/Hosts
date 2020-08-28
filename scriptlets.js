ff60003ac77a35e92c1b64139abdaa448fc45169
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-08-28 22:58:00 +0000

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -1,6 +1,8 @@
 /// test.js
 (function() {
     if ( window === window.top ) {
-        console.log("Test");
+        Object.defineProperty(navigator, 'userAgent', {
+    get: function () { return 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'; }
+});
     }
 })();
