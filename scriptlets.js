7990724ca93996f1fc7d9e8875c71e338a46c5d2
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-08-28 22:27:12 +0000

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -4,8 +4,8 @@
 (() => {
 'use strict'; 
 
-(function(useragent){
-Object.defineProperty(navigator, "userAgent", {enumerable: true, configurable: false, value: useragent});
-})('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36');
+Object.defineProperty(navigator, 'userAgent', {
+    get: function () { return 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'; }
+});
 
 })();
