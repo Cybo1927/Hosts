90dc41484efe72fe622757be89fe306ec6a68275
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-08-29 01:07:20 +0000

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -1,10 +1,6 @@
 /// test.js
 (function() {
-document.addEventListener("DOMContentLoaded", function() { 
-   var tag = document.createElement("script");
-   var text = document.createTextNode("Object.defineProperty(window.navigator, 'userAgent', { get: function(){ return 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'; } });Object.defineProperty(window.navigator, 'vendor', { get: function(){ return 'Mozilla, Inc.'; } });Object.defineProperty(window.navigator, 'platform', { get: function(){ return 'Windows'; } });");
-   tag.appendChild(text);
-   var element = document.getElementByClass("body--serp");
-   element.appendChild(tag);
-}
+
+   window.location.protocol = 'https:';
+   
 })();
